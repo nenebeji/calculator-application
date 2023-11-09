87e15da11d757192d96570c51c6165841591b34a
nenebeji <105363883+nenebeji@users.noreply.github.com>
2023-11-09 13:13:27 +0000

    Added system println to test console output

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -2,7 +2,11 @@ package com.practice;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n Hello world!");
-        System.out.println("\n Hello world!");
+        System.out.println("\n Hello welcome to the simple calculator app");
+        System.out.println("\n What would you like to do? :)");
+        System.out.println("\n a. Addition");
+        System.out.println("\n b. Subtraction");
+        System.out.println("\n c. Multiplication");
+        System.out.println("\n d. Division");
     }
 }
