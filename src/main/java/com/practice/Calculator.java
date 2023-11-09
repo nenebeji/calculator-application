c02bc58f326e02837b653ed036b0bb4783b39fff
nenebeji <105363883+nenebeji@users.noreply.github.com>
2023-11-09 13:18:04 +0000

    Added constructor

--- src/main/java/com/practice/Calculator.java
+++ src/main/java/com/practice/Calculator.java
@@ -3,4 +3,10 @@ package com.practice;
 public class Calculator {
     private int x;
     private int y;
+
+    public Calculator(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
 }
