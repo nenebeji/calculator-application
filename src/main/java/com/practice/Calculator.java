3ccd72f271a13bd0fcf9a31b1a1653d3126aff9d
nenebeji <105363883+nenebeji@users.noreply.github.com>
2023-11-09 13:24:07 +0000

    added calculator functions

--- src/main/java/com/practice/Calculator.java
+++ src/main/java/com/practice/Calculator.java
@@ -9,5 +9,37 @@ public class Calculator {
         this.y = y;
     }
 
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
+
+    public long add(long x, long y) {
+        return x + y;
+    }
+
+    public long subtract(long x, long y) {
+        return x - y;
+    }
+
+    public long multiply(long x, long y) {
+        return x * y;
+    }
+
+    public long divide(long x, long y) {
+        return x/y;
+    }
+
 
 }
