84f30cb486f48dd0fe7f3811b5064a2b3d5be6a8
nenebeji <105363883+nenebeji@users.noreply.github.com>
2023-11-09 13:48:39 +0000

    Added scanner obj to main

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -1,13 +1,18 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("\n Hello welcome to the simple calculator app");
         System.out.println("\n What would you like to do? :)");
         System.out.println("\n a. Addition");
         System.out.println("\n b. Subtraction");
         System.out.println("\n c. Multiplication");
         System.out.println("\n d. Division");
+        String option = scanner.nextLine();
+//        System.out.println(option);
 
         Calculator calculator = new Calculator(7.25, 9.9);
         calculator.add();
